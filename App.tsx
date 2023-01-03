a77b01eee68cdb208f1d02b5181dd3c7c19aff4d
moathalzoubi <32450805+moathalzoubi@users.noreply.github.com>
2023-01-03 10:28:08 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import TopBar from './src/Componaints/TopBar';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <TopBar />
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Cahtly
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-uzgqtt)
